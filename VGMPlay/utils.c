91a3829de073107b1adf3c8ea3698de6749371cc
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2015-02-27 14:05:27 -0300

    adding licensing headers

--- VGMPlay/utils.c
+++ VGMPlay/utils.c
@@ -1,5 +1,24 @@
-// This file is part of the VGMPlay program 
-// Licensed under the GNU General Public License version 2 (or later)
+/*
+ *  This file is part of VGMPlay <https://github.com/vgmrips/vgmplay>
+ *
+ *  (c)2015 Felipe C. da S. Sanches <juca@members.fsf.org>
+ *  (c)2015 Valley Bell
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 #include "utils.h"
 
--- VGMPlay/vgm2pcm.c
+++ VGMPlay/vgm2pcm.c
@@ -1,3 +1,25 @@
+/*
+ *  This file is part of VGMPlay <https://github.com/vgmrips/vgmplay>
+ *
+ *  (c)2015 Francis Gagné <fragag1@gmail.com>
+ *  (c)2015 Valley Bell
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #include <stdlib.h>
 #include <string.h>
 #include <wchar.h>
