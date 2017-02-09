60b6e86473f5f969358282548c71f852c5b21104
Valley Bell <valleybell@gmx.de>
2017-02-09 20:57:24 +0100

    make YM2151 clip to 10 bits per channel

--- VGMPlay/chips/ym2151.c
+++ VGMPlay/chips/ym2151.c
@@ -1752,8 +1752,8 @@ INLINE void chan_calc(unsigned int chan)
 	env = volume_calc(op+3);	/* C2 */
 	if (env < ENV_QUIET)
 		chanout[chan]    += op_calc(op+3, env, c2);
-	if (chanout[chan] > +8192)		chanout[chan] = +8192;
-	else if (chanout[chan] < -8192)	chanout[chan] = -8192;
+	if (chanout[chan] > +16384)			chanout[chan] = +16384;
+	else if (chanout[chan] < -16384)	chanout[chan] = -16384;
 
 	/* M1 */
 	op->mem_value = mem;
@@ -1818,8 +1818,8 @@ INLINE void chan7_calc(void)
 		if (env < ENV_QUIET)
 			chanout[7] += op_calc(op+3, env, c2);
 	}
-	if (chanout[7] > +8192)			chanout[7] = +8192;
-	else if (chanout[7] < -8192)	chanout[7] = -8192;
+	if (chanout[7] > +16384)		chanout[7] = +16384;
+	else if (chanout[7] < -16384)	chanout[7] = -16384;
 	/* M1 */
 	op->mem_value = mem;
 }
